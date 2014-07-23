380056563b7fc2f023a85f613c936ac6acf0c80f
Ryan Cangco <ryan.cangco@twistresources.com>
2014-07-23 17:56:57 +0800

    minor updates

--- copyJars.sh
+++ copyJars.sh
@@ -1,1 +1,6 @@
-cp ~/Development/Tooltwist-8.3.1/deployed-server/wtpwebapps/ttsvr/WEB-INF/lib/skypepi_t.jar lib/.
+
+read -p "Enter Jar Name: " jarName
+
+cp ~/Development/Tooltwist-8.3.1/deployed-server/wtpwebapps/ttsvr/WEB-INF/lib/$jarName lib/.
+
+echo "Copied $jarName"

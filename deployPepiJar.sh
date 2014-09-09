87b42c7f9be465145adf6b0488bdb053163b6dbe
studentportal <studentportal@bcskyuspp501.bctcms.org>
2014-09-09 12:28:45 +0100

    updates

---
+++ deployPepiJar.sh
@@ -0,0 +1,9 @@
+git pull origin prod
+jarName=skypepi_t.jar
+timestamp=$(date +%T)
+cp ~/server/tomcat/webapps/ttsvr/WEB-INF/lib/$jarName ~/backups/jarName$timestamp
+cp lib/$jarName ~/server/tomcat/webapps/ttsvr/WEB-INF/lib/.
+
+
+echo "Copied $jarName"
+

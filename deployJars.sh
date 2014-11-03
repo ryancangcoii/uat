1e8c95c31029b14b85abfb0368d3b030ed616529
Ryan Cangco <ryan.cangco@tooltwist.com>
2014-11-03 09:01:11 +0000

    deploy jar shell

--- deployJars.sh
+++ deployJars.sh
@@ -1,7 +1,7 @@
 read -p "Enter Jar Name: " jarName
 
 timestamp=$(date +%T)
-cp ~/server/tomcat/webapps/ttsvr/WEB-INF/lib/$jarName ~/backups/jarName$timestamp
+cp ~/server/tomcat/webapps/ttsvr/WEB-INF/lib/$jarName ~/backups/$jarName$timestamp
 cp lib/$jarName ~/server/tomcat/webapps/ttsvr/WEB-INF/lib/.
 
 
